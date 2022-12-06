e3a74cb2adce9a58e193e39f2129ee29a608ea83
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-12-06 15:34:37 +0700

    fix test case

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,36 +10,6 @@
 #include "input_module.h"
 #include "input_packet.h"
 #include "utility.h"
-#include "matoya.h"
-
-
-// Your top level application context
-struct context {
-	MTY_App *app;
-	bool quit;
-};
-
-// This function will fire for each event
-static void event_func(const MTY_Event *evt, void *opaque)
-{
-	struct context *ctx = (struct context *)opaque;
-
-	MTY_PrintEvent(evt);
-
-	if (evt->type == MTY_EVENT_CLOSE)
-		ctx->quit = true;
-}
-
-// This function fires once per "cycle", either blocked by a
-// call to MTY_WindowPresent or limited by MTY_AppSetTimeout
-static bool app_func(void *opaque)
-{
-	struct context *ctx = (struct context *)opaque;
-
-	MTY_WindowPresent(ctx->app, 0);
-
-	return !ctx->quit;
-}
 
 /**
  * Test cases:
@@ -96,23 +66,6 @@ int main(int argc, char *argv[]) {
         f(&t);
     }
     t.print_test_result();
-
-    // Set up the application object and attach it to your context
-	struct context ctx = {0};
-	ctx.app = MTY_AppCreate(app_func, event_func, &ctx);
-	if (!ctx.app)
-		return 1;
-
-	// Create a window
-	MTY_WindowCreate(ctx.app, "My Window", NULL, 0);
-
-	// Set the graphics API to OpenGL
-	MTY_WindowSetGFX(ctx.app, 0, MTY_GFX_GL, true);
-
-	// Run the app -- blocks until your app_func returns false
-	MTY_AppRun(ctx.app);
-	MTY_AppDestroy(&ctx.app);
-
     return 0;
 }
 
@@ -136,36 +89,36 @@ void test_rel_mouse(void* t) {
     packet.magic = util::endian::little(magic);
 
     // move right
-    packet.deltaX = util::endian::big((short)500); 
+    packet.deltaX = util::endian::big((short)5); 
     packet.deltaY = util::endian::big((short)0);
     std::cout << "Send packet 1" << std::endl;
     inpmod_print(in, (uint8_t *)&packet);
     inpmod_passthrough(in, (uint8_t *)&packet, sizeof(packet));
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
     // move down
     packet.deltaX = util::endian::big((short)0); 
-    packet.deltaY = util::endian::big((short)500);
+    packet.deltaY = util::endian::big((short)5);
     std::cout << "Send packet 2" << std::endl;
     inpmod_print(in, (uint8_t *)&packet);
     inpmod_passthrough(in, (uint8_t *)&packet, sizeof(packet));
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
     // move left
-    packet.deltaX = util::endian::big((short)-500); 
+    packet.deltaX = util::endian::big((short)-5); 
     packet.deltaY = util::endian::big((short)0);
     std::cout << "Send packet 3" << std::endl;
     inpmod_print(in, (uint8_t *)&packet);
     inpmod_passthrough(in, (uint8_t *)&packet, sizeof(packet));
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
     // move up
     packet.deltaX = util::endian::big((short)0); 
-    packet.deltaY = util::endian::big((short)-500);
+    packet.deltaY = util::endian::big((short)-5);
     std::cout << "Send packet 4" << std::endl;
     inpmod_print(in, (uint8_t *)&packet);
     inpmod_passthrough(in, (uint8_t *)&packet, sizeof(packet));
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
     inpmod_wait(in, 30);
 
